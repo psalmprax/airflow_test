8928cf3ad96d0a5fe546a2fdb9ac1feccefb0bf0
psalmprax <samuelolle@yahoo.com>
2022-06-21 06:32:22 -0700

    test data scrape without vpn

--- dags/docker_job/scrape.py
+++ dags/docker_job/scrape.py
@@ -106,6 +106,19 @@ def create_webdriver(browser, proxy_config):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('user-agent={0}'.format(user_agent))
+        chrome_options.add_argument('--disable-infobars')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("start-maximized")
+
+        chrome_options.add_argument("--dns-prefetch-disable")
+        chrome_options.add_argument("---force-device-scale-factor=1")
+
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+        chrome_options.add_argument('--incognito')
+
+        chrome_options.add_argument("log-level=3")
         driver = get_webdriver_for(browser, options=chrome_options, desired_capabilities=proxy_config)
 
     return driver
