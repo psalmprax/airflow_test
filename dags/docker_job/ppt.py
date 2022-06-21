3e04369a772dcddccccf93d62851c9b4f2b0c178
psalmprax <samuelolle@yahoo.com>
2022-06-21 04:49:43 -0700

    test data scrape without vpn

---
+++ dags/docker_job/ppt.py
@@ -0,0 +1,239 @@
+from __future__ import print_function
+
+import random
+import time
+from datetime import timedelta, datetime, date
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+# from airflow.hooks.postgres_hook import PostgresHook
+from bs4 import BeautifulSoup
+# from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from docker_job.pricing.vars import *  # iphones
+from docker_job.scrape import *
+
+
+class ProductPriceData:
+    driver: None
+
+    def __init__(self, driver=None):
+        self.driver = driver
+
+    def get_product_cat_url(self, url, driver):
+        """
+            url: product url to get the pricing details
+        Returns:
+            object:
+        """
+        lst = list()
+        self.driver = driver
+        self.driver.get(url)
+        time.sleep(5)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        for div in soup.findAll("div", {"class": "uk-width-1-2"}):
+            lst.append(div.find("a", href=True).get('href'))
+        self.driver.quit()
+
+        return lst
+
+    @classmethod
+    def clean_list(cls, lst, string):
+        """
+            (1) take out 'gebraucht-kaufen' because we only want sourcing (verkaufen not kaufen)
+            (2) take out '//'
+            (3) Using format()
+            (4) append https so the string functions as a url
+        Returns:
+            object:
+        """
+        lst = [string + '{0}'.format(i) for i in [ele.strip('//')
+                                                  for ele in [x for x in lst if "gebraucht-kaufen" not in x]]]
+        return lst
+
+    def get_prices(self, driver, clickables):
+        """
+        Returns:
+            object:
+        """
+        self.driver = driver
+        prices, anbieters = list(), list()
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        get_prices = soup.findAll("span", {"class": "uk-hidden-small uk-h1 uk-text-primary pricetag"})
+        device = soup.find("h1", class_="handy_name").text
+        text = self.driver.find_element_by_xpath(clickables).get_attribute('textContent').strip()
+
+        if "AM" in datetime.today().strftime("%D:%M:%Y:%H:%M %p"):
+            time_of_day = "AM"
+        else:
+            time_of_day = "PM"
+
+        for href in get_prices:
+            anbieter = href.find("a", href=True)
+            prices.append(anbieter.text)
+            anbieters.append(anbieter.get('href'))
+
+        dct = {'Prices': prices,
+               'Anbieter': anbieters,
+               'Device': [device] * len(anbieters),
+               'Condition': [text] * len(anbieters),
+               'Created_at': [date.today()] * len(anbieters),
+               'Am_Pm': [time_of_day] * len(anbieters)}
+        return dct
+
+    # def (self, urls, clickable_links, driver, xpath ):
+    def scrape_phones(self, urls, driver, clickable_links, xpath):
+        dfs = list()
+        self.driver = driver
+        clickable_links = clickable_links
+        xpath = xpath
+        for url in urls:
+            try:
+                for val_class in [2, 3, 4, 5, 1]:
+                    if val_class == 2:
+                        # sleep for a random time between 2 & 5 secs before scraping next url
+                        time.sleep(random.uniform(2, 5))
+                        self.driver.get(url)
+
+                    # open dropdown menu again
+                    WebDriverWait(self.driver, 20).until(
+                        EC.element_to_be_clickable((By.XPATH, clickable_links))).click()
+
+                    # click conditiona status (sehr gut,gut, ****)
+                    self.driver.find_element_by_xpath(xpath.format(val_class)).click()
+                    time.sleep(5)
+                    status = self.get_prices(self.driver, clickable_links)
+                    dfs.append(status)
+            except Exception as ex:
+                print(ex)
+
+        self.driver.quit()
+
+        return dfs
+
+
+def run_from_class(**kwargs):
+    from docker_job.pricing.vars import options, clickables, xpath
+    from airflow.hooks.postgres_hook import PostgresHook
+
+    # pg_hook = PostgresHook(postgres_conn_id='pricing')
+    # connection = pg_hook.get_conn()
+    # cursor = connection.cursor()
+
+    ppd = ProductPriceData()
+    result = list()
+    print(kwargs["url"])
+    # phone_data = ppd.scrape_phones([kwargs["url"]],
+    #                                driver,
+    #                                clickables, xpath)
+    phone_data = main(kwargs["url"], ppd)
+    result += phone_data
+    results = [(a.strip("€"), b, c, d, str(e), f) for row in result
+               for a, b, c, d, e, f in zip(row['Prices'],
+                                           row['Anbieter'],
+                                           row['Device'],
+                                           row['Condition'],
+                                           row['Created_at'],
+                                           row['Am_Pm'])]
+    # insert_query = "insert into analytics_objects.obj_product_price_stage (price,anbieter,device,condition," \
+    # "created_at) values {} "
+    # insert_query = insert_query.format(','.join(['%s'] * len(results)))
+    # cursor.execute(insert_query, results)
+    # cursor.execute("COMMIT")
+    print(results)
+    # return {"data": result}
+
+
+# noinspection PyShadowingNames
+def create_dag(
+        dag_id,
+        schedule,
+        task_id,
+        description,
+        start_date,
+        default_args,
+        my_func
+):
+    dag = DAG(schedule_interval=schedule,
+              default_args=default_args,
+              description=description,
+              dag_id=dag_id,
+              start_date=start_date,
+              catchup=False,
+              template_searchpath=['/opt/airflow/dags'])
+    with dag:
+        # python_task = PythonOperator(
+        #     task_id=task_id,
+        #     provide_context=True,
+        #     python_callable=my_func
+        # )
+
+        # create_product_price_table = PostgresOperator(
+        #     task_id=f"table_{task_id}_creation",
+        #     postgres_conn_id="pricing",
+        #     sql="""sql/create_product_price_table.sql"""
+        # )
+        #
+        # upsert_product_price_table = PostgresOperator(
+        #     task_id=f"table_{task_id}_upsert",
+        #     postgres_conn_id="pricing",
+        #     sql="""sql/upsert_product_price_table.sql"""
+        # )
+
+        start_clean = BashOperator(
+            task_id=f'start_clean-{task_id}',
+            bash_command=f"pkill chrome 2>/dev/null || exit 0 "
+                         f"&& rm -rf /tmp/.* 2>/dev/null || exit 0"
+                         f"&& rm -rf /tmp/* 2>/dev/null || exit 0"
+                         f"&& rm -rf /tmp/.p* 2>/dev/null || exit 0"
+        )
+
+        end_clean = BashOperator(
+            task_id=f'end_clean-{task_id}',
+            bash_command=f"pkill chrome 2>/dev/null || exit 0 "
+                         f"&& rm -rf /tmp/.* 2>/dev/null || exit 0"
+                         f"&& rm -rf /tmp/* 2>/dev/null || exit 0"
+                         f"&& rm -rf /tmp/.p* 2>/dev/null || exit 0"
+        )
+        for url in iphones[:10]:
+            ti = url.split("/")[-1]
+            print(ti)
+            python_task = PythonOperator(
+                task_id=f"{task_id}-{ti}",
+                provide_context=True,
+                python_callable=my_func,
+                op_kwargs={"url": url}
+            )
+            # >> update_product_price_table , create_product_price_table >> ,upsert_product_price_table >>
+            start_clean >> python_task >> end_clean
+    return dag
+
+
+task_id = "ppt"
+
+default_args = {
+    'depends_on_past': False,
+    'email': ['samuel.momoh-olle@asgoodasnew.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2),
+    'execution_timeout': timedelta(seconds=2000),
+}
+description = f'A {task_id} DAG '
+schedule_interval = timedelta(minutes=150)
+dag_id = f'Job-{task_id}'
+start_date = datetime(2022, 6, 15)
+
+globals()[dag_id] = create_dag(dag_id=dag_id,
+                               schedule=schedule_interval,
+                               task_id=task_id,
+                               description=description,
+                               start_date=start_date,
+                               default_args=default_args,
+                               my_func=run_from_class)
---
+++ dags/docker_job/scrape.py
@@ -0,0 +1,151 @@
+from itertools import cycle
+# from time import sleep
+import time as t
+from raw_copy import *
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.proxy import ProxyType, Proxy
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+from webdriver_manager.chrome import ChromeDriverManager
+from parameters import iphones
+from webdriver_setup import get_webdriver_for
+
+clickables = "//button[@id='buttonZustand']"
+xpath = "//ul[@class='uk-nav uk-nav-dropdown uk-text-bold' and @id='dropdownZustand']//li[@class='{} " \
+        "uk-dropdown-close'] "
+
+def get_proxies():
+    """Return the list of elite proxies
+    :rtype: list
+    :returns: List of proxies
+    """
+
+    url = "https://www.sslproxies.org/"
+
+    response = requests.get(url)
+    parsed_page = BeautifulSoup(response.content, "html.parser")
+
+    ip_table = parsed_page.find(id="list")
+    rows = ip_table.select("tbody > tr")
+
+    # filter proxies whose last checked time is less than 1 hour
+    rows_for_last_hour = [row for row in rows if "hour" not in row.select("td:nth-of-type(8)")[0].text]
+
+    proxies = []
+    # print(rows_for_last_hour)
+    for row in rows_for_last_hour:
+        # print(row.contents)
+        print(row.select("td:nth-of-type(5)")[0].text)
+        anonymity = row.select("td:nth-of-type(5)")[0].text
+
+        if "elite" in anonymity:
+            host = row.select("td:nth-of-type(1)")[0].text
+            port = row.select("td:nth-of-type(2)")[0].text
+            proxy = f"{host}:{port}"
+            proxies.append(proxy)
+
+    return proxies
+
+
+def setup_proxy(browser, proxy):
+    """Make proxy settings for the given browser
+    :type browser: str
+    :param browser: Name of the browser
+    :type proxy: str
+    :param proxy: Proxy string in the host:port form
+    :rtype: FirefoxOptions or DesiredCapabilities
+    :returns: FirefoxOptions for Firefox browser, capabilities for Chrome browser
+    """
+
+    print(f"Switching proxy to {proxy}...")
+
+    if browser == "chrome":
+        proxy_config = Proxy()
+        proxy_config.proxy_type = ProxyType.MANUAL
+        proxy_config.http_proxy = proxy
+        proxy_config.ssl_proxy = proxy
+
+        capabilities = DesiredCapabilities.CHROME.copy()
+        proxy_config.add_to_capabilities(capabilities)
+
+        return capabilities
+
+    else:
+        raise SystemExit("Invalid browser! Should be firefox or chrome.")
+
+
+def create_webdriver(browser, proxy_config):
+    """Create webdriver instance for the given browser and proxy setting
+    :type browser: str
+    :param browser: Name of the browser
+    :type proxy_config: FirefoxOptions or DesiredCapabilities
+    :param proxy_config: Proxy configuration
+    :rtype: selenium.webdriver
+    :returns: Firefox or Chrome webdriver instance
+    """
+
+    if browser == "firefox":
+        firefox_options = proxy_config
+        firefox_options.headless = True
+        driver = get_webdriver_for(browser, options=firefox_options)
+
+    elif browser == "chrome":
+        ChromeDriverManager().install()
+
+        softwares_names = [SoftwareName.CHROME.value]
+        operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+        user_agent_rotator = UserAgent(softwares_names=softwares_names,
+                                       operatiing_systems=operating_systems,
+                                       limit=10000000)
+        user_agent = user_agent_rotator.get_random_user_agent()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('user-agent={0}'.format(user_agent))
+        driver = get_webdriver_for(browser, options=chrome_options, desired_capabilities=proxy_config)
+
+    return driver
+
+
+def make_request_with_proxy(browser, proxy_config, urls, ppd):
+    """Make a request to check new IP
+    Skip if connection fails.
+    :type browser: str
+    :param browser: Name of the browser
+    :type proxy_config: FirefoxOptions or DesiredCapabilities
+    :param proxy_config: Proxy configuration
+    """
+
+    driver = create_webdriver(browser, proxy_config)
+    print(proxy_config)
+    t.sleep(1)
+
+    try:
+
+        ppd.scrape_phones(urls=urls, driver=driver, clicks=clickables, xpath=xpath)
+        # print(f"IP: {driver.find_element(By.ID, 'ip').text}")
+
+    except WebDriverException as ex:
+        print("Connection error! Skipping...", ex)
+
+    driver.quit()
+
+
+def main(urls, ppt):
+
+    proxies = get_proxies()
+    proxy_pool = cycle(proxies)
+
+    browser = "chrome"
+
+    for _ in range(20):
+        proxy = next(proxy_pool)
+        proxy_config = setup_proxy(browser, proxy)
+        make_request_with_proxy(browser, proxy_config, urls, ppt)
+        t.sleep(20)
+
+# main()
