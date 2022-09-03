57d719ae1e661eb0ed9c2297c56abf29d4b05773
psalmprax <samuelolle@yahoo.com>
2022-09-03 15:04:10 -0700

    Official first stage deployment of the airflow and dbt ETL jobs v20

--- airflow_connections.sh
+++ airflow_connections.sh
@@ -6,7 +6,7 @@ airflow connections add 'mysql_conn' \
     --conn-login 'data-analytics' \
     --conn-host 'db.warehouse.asgoodasnew.com' \
     --conn-port '3306' \
-    --conn-schema 'adamant';
+    --conn-schema 'adamant' 2> /dev/null;
 
 sudo docker exec airflow_test_airflow-worker_1 \
 airflow connections add 'mysql_wkfs_conn' \
@@ -14,7 +14,7 @@ airflow connections add 'mysql_wkfs_conn' \
     --conn-login 'data-analytics' \
     --conn-host 'wkfs-production-slave3.cfkok039naoz.eu-central-1.rds.amazonaws.com' \
     --conn-port '3306' \
-    --conn-schema 'symfony1';
+    --conn-schema 'symfony1' 2> /dev/null;
 
 sudo docker exec airflow_test_airflow-worker_1 \
 airflow connections add 'mysql_scanpoint_conn' \
@@ -22,7 +22,7 @@ airflow connections add 'mysql_scanpoint_conn' \
     --conn-login 'data-analytics' \
     --conn-host 'wkfs-production-slave3.cfkok039naoz.eu-central-1.rds.amazonaws.com' \
     --conn-port '3306' \
-    --conn-schema 'scanpoint';
+    --conn-schema 'scanpoint' 2> /dev/null;
 
 sudo docker exec airflow_test_airflow-worker_1 \
 airflow connections add 'psql_conn' \
@@ -30,13 +30,13 @@ airflow connections add 'psql_conn' \
     --conn-login 'master' \
     --conn-host '11.3.0.226' \
     --conn-port '5432' \
-    --conn-schema 'dwh';
+    --conn-schema 'dwh' 2> /dev/null;
 
 sudo docker exec airflow_test_airflow-worker_1 \
 airflow connections add 'profiles_key' \
     --conn-type 'fs' \
-    --conn-login 'profiles_key';
+    --conn-login 'profiles_key' 2> /dev/null;
 
 sudo docker exec airflow_test_airflow-worker_1 \
 airflow connections add 's3_conn' \
-    --conn-type 's3';
+    --conn-type 's3' 2> /dev/null;
